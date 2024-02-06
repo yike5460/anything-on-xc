befca52d14453763e1b83906965aa175a8b362d7
yike5460 <yike5460@163.com>
2024-02-06 02:45:46 +0000

    chore: user data update

--- source/infrastructure/lib/common/user_data.sh
+++ source/infrastructure/lib/common/user_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 echo "Starting user_data.sh and installing dependencies"
 
-set -euxo pipefail
+# set -euxo pipefail
+# remove u option since we need the string replacement to work in the following line
+set -exo pipefail
 
 sudo apt-get update
 sudo apt install software-properties-common -y
@@ -31,50 +33,55 @@ sudo chown ubuntu:ubuntu /tmp/s3-mount
 sudo mkdir -p /tmp/efs-mount
 sudo chown ubuntu:ubuntu /tmp/efs-mount
 
-s3fs $BUCKET_NAME /tmp/s3-mount -o passwd_file=${TMP_FOLDER}/.passwd-s3fs & > ${TMP_FOLDER}/s3fs.log
-echo "S3 bucket $BUCKET_NAME mounted at /tmp/s3-mount"
+# var of the bucket name
+BUCKET_NAME="placeholder"
+FS_ID="placeholder"
+REGION="placeholder"
+
+s3fs ${BUCKET_NAME} /tmp/s3-mount -o passwd_file=${TMP_FOLDER}/.passwd-s3fs & > ${TMP_FOLDER}/s3fs.log
+echo "S3 bucket ${BUCKET_NAME} mounted at /tmp/s3-mount"
 
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${FS_ID}.efs.${REGION}.amazonaws.com:/ /tmp/efs-mount
-echo "EFS file system $FS_ID mounted at /tmp/efs-mount"
+echo "EFS file system ${FS_ID} mounted at /tmp/efs-mount"
 
-# cd /home/ubuntu
+cd /home/ubuntu
 
-# curl -sSL https://raw.githubusercontent.com/awslabs/stable-diffusion-aws-extension/main/install.sh | bash
+curl -sSL https://raw.githubusercontent.com/awslabs/stable-diffusion-aws-extension/main/install.sh | bash
 
-# cd stable-diffusion-webui/extensions/stable-diffusion-aws-extension
-# git checkout main
-# cd ../../
+cd stable-diffusion-webui/extensions/stable-diffusion-aws-extension
+git checkout main
+cd ../../
 
-# wget -qP models/Stable-diffusion/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Stable-diffusion/sd_xl_base_1.0.safetensors
-# wget -qP models/Stable-diffusion/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors
-# wget -qP models/ControlNet/ https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
-# wget -qP models/ControlNet/ https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
-# wget -qP models/Lora/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Lora/lcm_lora_xl.safetensors
-# wget -qP models/Lora/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Lora/lcm_lora_1_5.safetensors
+wget -qP models/Stable-diffusion/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Stable-diffusion/sd_xl_base_1.0.safetensors
+wget -qP models/Stable-diffusion/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors
+wget -qP models/ControlNet/ https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
+wget -qP models/ControlNet/ https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
+wget -qP models/Lora/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Lora/lcm_lora_xl.safetensors
+wget -qP models/Lora/ https://aws-gcr-solutions-us-east-1.s3.us-east-1.amazonaws.com/extension-for-stable-diffusion-on-aws/models/Lora/lcm_lora_1_5.safetensors
 
-# sudo chown -R ubuntu:ubuntu /home/ubuntu/stable-diffusion-webui
+sudo chown -R ubuntu:ubuntu /home/ubuntu/stable-diffusion-webui
 
-# cat > sd-webui.service <<EOF
-# [Unit]
-# Description=Stable Diffusion UI server
-# After=network.target
-# StartLimitIntervalSec=0
+cat > sd-webui.service <<EOF
+[Unit]
+Description=Stable Diffusion UI server
+After=network.target
+StartLimitIntervalSec=0
 
-# [Service]
-# WorkingDirectory=/home/ubuntu/stable-diffusion-webui
-# ExecStart=/home/ubuntu/stable-diffusion-webui/webui.sh --enable-insecure-extension-access --skip-torch-cuda-test --no-half --listen
-# Type=simple
-# Restart=always
-# RestartSec=3
-# User=ubuntu
-# StartLimitAction=reboot
+[Service]
+WorkingDirectory=/home/ubuntu/stable-diffusion-webui
+ExecStart=/home/ubuntu/stable-diffusion-webui/webui.sh --enable-insecure-extension-access --skip-torch-cuda-test --no-half --listen
+Type=simple
+Restart=always
+RestartSec=3
+User=ubuntu
+StartLimitAction=reboot
 
-# [Install]
-# WantedBy=default.target
+[Install]
+WantedBy=default.target
 
-# EOF
-# sudo mv sd-webui.service /etc/systemd/system
-# sudo chown root:root /etc/systemd/system/sd-webui.service
+EOF
+sudo mv sd-webui.service /etc/systemd/system
+sudo chown root:root /etc/systemd/system/sd-webui.service
 
-# sudo systemctl enable sd-webui.service
-# sudo systemctl start sd-webui.service
+sudo systemctl enable sd-webui.service
+sudo systemctl start sd-webui.service
